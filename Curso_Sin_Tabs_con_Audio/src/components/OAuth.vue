6837f72bb7ed1b97a9cf4aa499472cb1b9d64183
Eric Trejo <ericto117@gmail.com>
2021-01-14 19:28:09 -0600

    Curso 14/01/2021

--- Curso_Sin_Tabs_con_Audio/src/components/OAuth.vue
+++ Curso_Sin_Tabs_con_Audio/src/components/OAuth.vue
@@ -2,31 +2,47 @@
   <div class="section">
     <div class="section has-text-centered">
       <div class="section">Procesando: {{ this.code }}</div>
+      Query
+      <b-input v-model="query" />
+      <b-button @click="this.buscar">Buscar</b-button>
+      <b-table
+        v-if="data"
+        @click="
+          (item) => {
+            this.reproducir(item.id);
+          }
+        "
+        :data="data.data"
+        :columns="data.columns"
+      ></b-table>
     </div>
   </div>
 </template>
 
 <script>
-const url = "https://freesound.org/apiv2/oauth2/access_token/";
+const prefijo = "https://freesound.org/apiv2/";
+const url = prefijo + "oauth2/access_token/";
+const urlbusqueda = prefijo + "search/text/";
+const urldescarga = prefijo + "sounds/";
 export default {
   data() {
     return {
       code: null,
+      query: "",
+      token: "",
+      data: null,
     };
   },
   beforeMount() {
-    this.code = this.$route.query.code;
-    this.intercambiar();
+    if (!localStorage.getItem("token")) {
+      this.code = this.$route.query.code;
+      this.intercambiar();
+    } else {
+      this.token = localStorage.getItem("token");
+    }
   },
   methods: {
     intercambiar() {
-      /*
-      var data = {
-        client_id: "Azk196NWwiPosN9Sclpj",
-        client_secret: "HqXUXX2vkuBw5q95KtWXb2R5tjZzC4e1i3nwdVc2",
-        grant_type: "authorization_code",
-        code: this.code,
-      };*/
       var formData = new FormData();
       formData.append("client_id", "Azk196NWwiPosN9Sclpj");
       formData.append(
@@ -39,27 +55,58 @@ export default {
         .then((response) => response.json())
         .catch((error) => console.error("Error:", error))
         .then(async (respuesta) => {
-          const url2 = "https://freesound.org/apiv2/sounds/14854/download/";
-          const auth = "Bearer " + respuesta.access_token;
-          const audio = await fetch(url2, {
-            method: "GET",
-            headers: {
-              Authorization: auth,
-            },
-          })
-            .then(async (response) => {
-              let reader = response.body.getReader();
-              return reader.read().then((result) => {
-                return result;
-              });
-            })
-            .catch((error) => console.error("Error:", error));
+          if (respuesta.access_token) {
+            this.persistencia(respuesta.access_token);
+          }
+        });
+    },
+    buscar() {
+      var urlnuevo = urlbusqueda + "?query=" + this.query;
+      console.log(urlnuevo);
+      fetch(urlnuevo, {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + this.token,
+        },
+      })
+        .then((response) => response.json())
+        .catch((error) => console.error("Error:", error))
+        .then((respuesta) => {
+          var info = {
+            data: respuesta.results,
+            columns: [
+              {
+                field: "name",
+                label: "Nombre",
+                centered: true,
+              },
+            ],
+          };
+          this.data = info;
+        });
+    },
+    persistencia(token) {
+      localStorage.setItem("token", token);
+    },
+    async reproducir(id) {
+      var urldescargaunico = urldescarga + id + "/download/";
+      fetch(urldescargaunico, {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + this.token,
+        },
+      })
+        .then(async (response) => {
+          let reader = response.body.getReader();
+          var audio = await reader.read();
+          console.log("AUDIO OK");
           var blob = new Blob([audio.value], { type: "audio/mp3" });
           var url = window.URL.createObjectURL(blob);
           window.audio = new Audio();
           window.audio.src = url;
           window.audio.play();
-        });
+        })
+        .catch((error) => console.error("Error:", error));
     },
   },
 };
